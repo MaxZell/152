ebfcf165843dedbdca0ca1ff14b518e949918cdf
mzelensky <maxim.zelensky@soxes.ch>
2021-03-05 13:20:56 +0100

    impressum

--- src/Impressum.js
+++ src/Impressum.js
@@ -1,7 +1,32 @@
+import "./Impressum.css"
+
 export function createImpressum() {
     return (
         <div className="impressum">
             <h2>Impressum</h2>
+            <p>
+                If you have any questions about the site, please contact Maxim Zelensky at the contact information below.
+            </p>
+            <p>
+                Wenn Sie Fragen zur Website haben, wenden Sie sich bitte an Maxim Zelensky über die unten stehenden Kontaktangaben.
+            </p>
+            <tr>
+                <th className="title">&#128100;</th>
+                <th>Maxim Zelensky</th>
+            </tr>
+            <tr>
+                <th className="title">&#8962;</th>
+                <th>Sennweidstrasse 1b, 8608 Bubikon</th>
+            </tr>
+            <tr>
+                <th className="title">&#128231;</th>
+                <th>maxim.zelensky@soxes.ch</th>
+            </tr>
+            <tr>
+                <th className="title"><img height="20px" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAFLUlEQVR4Xu2bf0xVZRjHv+/ldygQKqABQZPJBb1EtEVlCLYkqEG16WatmomBzWFmS7PlSleatQQnE5N+LWsqNRcSNFDEX8lW4g9EAQMUQrgFF4ELXPBy3vYeh7nL9Zz3sjbOuZz3v7P7Pe99ns95z/O8P85DYNP0WcVzKYQVRMBTlCAUgLetRmXX/YSihepQTqDbczn/2Yt32k9GL6IXH3C3+ntuJ0AWAJ3KnOQ1V6BAvqvJsqa2cMkwu0kEwJwf8fcsBbCQtyeV6ypcTJYUBkEEMCezKI8Ab6jcKQfNp3l1u9NXEfGdp8J5Jx72dwMjEKKLIZFZRbmgyHYQn1PICUgu0b9edIkS6Hk88vZ0xSx/L+gIQXv3IHoHbvLcplgNobhMIjOLzHKpLjxwCtYvjsbjUTPgoruVOARKceaKCR8XXkRda69inZQxzMwAUCnRc4+GYNNLBri52s+MIwLFlgO1+L6yWZUQJAGEBXrj4HsL4OnuIuncTauAJVtPoO4v9Y0ESQDbV8Th6bhZXE+27Gw7Vu/+g0urJJEkgFOfJsN/qjuXvR3dg0h69zCXVkkiSQDVuanw8pAe/qPOsKAYs6oE1hFBSf7J2iIJYP+6+TCE3yvbCRM0dZjxzAdHubRKEkkCWPu8HhnJs7ns3Xf8Gj784QKXVkkiSQBs4lO0MVGc/Ei1v3ssSNtUiZ5+9U2MZOcB+hBffLb8ITwQNMUug2ajGeu+PouaqzeU9GC5bZEFwHpyd9Xh5YXhSDIE4b5pXmCL6utdA6isMeK7imYMW9UV+O6kwwWAG6cKhRoAubWACh+qQyarYgSwtUh0qC8C/Dzh4+WG/iEruvqGcdVoRrtp0CGHbcWSAB6OmIYXE8O4/mBvRTOqG01jtFkpEdCH+nL1YbuWmBfmh8yUCCTOC7y9DLftiGWhivNGfHO4EZ29Q1z/wx0E0x4JxifLYrk6ffvLavzye9sY7Z7seMyPmsHVhz7rkKhj648tr8YiYW4A131MNDg0gq/KG7GrpAFsic7bJEfARAAInn4PCrLjcX/A+I4jjtUY8VZBNQaGrFwMFAUgfu2v4v7DTJmZp5xnVfWdWJ5TJe5ayTVFATh6wYgkQ6CczVy/5/5ch/zSK7JaRQGQtdYBAYsDye9XoK1rQPIupwXAvP72SBO2FtZOXgBmixVPvFMGy/DIXSEocgSw4bv/+DWUn2vHn9f7ROPnBPtg6YIwPBkT5MCLACzLOY2quk71ADD1DeO1nNOob7O/w8wmRm+mR3JD2Flcj7ziBnUAYE9+6baTsnsLP25IEKfGPO3UpX+QsaNKHQAKT7Zg4152TivdXngsBB+98qCcTPy9oa0X6ZuPqQMAe/rnmrplHfNw06F6R6p4RinXjDcsSFxfrg4AcatLuaewJ7YtwnQfDzn/xQwQm12iDgBRKw+BY/YqOvPThgREccaB0UWWPQqKSoNShtoazxZM7LSapzklgPEss51qBGgAxrHRoo0AOwRUGwS1V0B7BRzfbNVigBYDxhLQgqDU2eBEnAvwTG2ZRssCWhbQsoDDZ45aGtTSoINp0BDmh7T4YK7AfPC3VtS29IzRsr382TOncvWxeV8Nl46J/q9+VfGFCDeVcQi5CibG0a9abjE7VDKjFq947bxVMjPpi6YmcdmcToBBK5xk78tkK52lwBFXkyX1dunsfxA8PgfISieuIhUAusubdqw580Wm+G3/mNNFVkoLSjNA6SJKEALA/nfyvKF24nVmQtEKQspASIFt+fy/VnI5hxahcpUAAAAASUVORK5CYII=" alt="Linkedin" />
+                </th>
+                <th>www.linkedin.com/in/maxim-zelensky-66693a176/</th>
+            </tr>
         </div>
     );
 }

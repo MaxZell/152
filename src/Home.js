e797a713f15c7c0e0ca3f8ed777ae930e9aabb51
mzelensky <maxim.zelensky@soxes.ch>
2021-03-19 12:58:34 +0100

    label added

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import { App } from "./Autocomplete";
+// import React, {Suspense } from 'react';
+// const App = React.lazy(() => import('./Autocomplete'), App => App.default);
 
 function Ani(){
     return App();
@@ -9,6 +11,9 @@ export function createHome(){
         <div className="home">
             <h2>Home</h2>
             <div className="Title">MaxZel Webseite</div>
+            {/* <Suspense fallback={<div>Loading...</div>}>
+                <App />
+            </Suspense> */}
             <Ani />
         </div>
     );

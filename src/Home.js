b6f1b439c95731ef0dd0ea947af9d34fe99a7622
mzelensky <maxim.zelensky@soxes.ch>
2021-03-19 11:14:41 +0100

    contries autocoplete added

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
+import { App } from "./Autocomplete";
+
+function Ani(){
+    return App();
+}
+
 export function createHome(){
     return (
         <div className="home">
             <h2>Home</h2>
             <div className="Title">MaxZel Webseite</div>
+            <Ani />
         </div>
     );
 }

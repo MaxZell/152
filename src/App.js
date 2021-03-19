5f727bf803ad802c1dcae1ddf23d84f0ee890673
mzelensky <maxim.zelensky@soxes.ch>
2021-03-19 10:01:22 +0100

    new routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-
 import React, { Component } from 'react';
 import { HashRouter, Route, Link } from "react-router-dom";
+
+import './styles/App.css';
+
+import CookieConsent from "react-cookie-consent";
 import { createImpressum } from "./Impressum";
+import { createStyleguide } from "./Styleguide";
+import { createGallery } from "./formatsGallery";
+import { createFormats } from "./Formats";
+import { createCopyrights } from "./Copyrights";
+import { createIFrame } from "./DSGVO";
+
+function setActive(name) {
+  console.log(`tab: ${name}`);
+}
 
 class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/impressum">Impressum</Link></li>
-          </ul>
-
-          <hr />
-  
-          <Route exact path="/" component={Home} />
-          <Route path="/impressum" component={Impressum} />
+        <div className="App">
+          <CookieConsent
+            location="bottom"
+            buttonText="OK"
+            cookieName="Cookie"
+            style={{ background: "#2B373B" }}
+            buttonStyle={{ width: "100px", color: "#4e503b", fontSize: "13px"}}
+            expires={150}
+          >
+            This website uses cookies to enhance the user experience.
+          </CookieConsent>
+          <div className="Main">
+            <nav>
+                <ul>
+                  <li onClick={() => setActive('home')}><Link to="/">Home</Link></li>
+                  <li onClick={() => setActive('impressum')}><Link to="/impressum">Impressum</Link></li>
+                  <li onClick={() => setActive('styleguide')}><Link to="/styleguide">Styleguide</Link></li>
+                  <li onClick={() => setActive('gallery')}><Link to="/gallery">Gallery</Link></li>
+                  <li onClick={() => setActive('formats')}><Link to="/formats">Media Formats</Link></li>
+                  <li onClick={() => setActive('copyrights')}><Link to="/copyrights">Copyrights</Link></li>
+                  <li onClick={() => setActive('dsgvo')}><Link to="/dsgvo">DSGVO</Link></li>
+                </ul>
+              </nav>
+    
+            <Route exact path="/" component={Home} />
+            <Route path="/impressum" component={Impressum} />
+            <Route path="/styleguide" component={Styleguide} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/formats" component={Formats} />
+            <Route path="/copyrights" component={Copyrights} />
+            <Route path="/dsgvo" component={Dsgvo} />
+          </div>
         </div>
       </HashRouter>
     );
@@ -25,5 +59,10 @@ class App extends Component {
 
 const Home = () => <h2>Home</h2>
 const Impressum = () => createImpressum();
+const Styleguide = () => createStyleguide();
+const Gallery = () => createGallery();
+const Formats = () => createFormats();
+const  Copyrights= () => createCopyrights();
+const  Dsgvo= () => createIFrame();
 
 export default App;
